b8e5c6867585dd4cb137f0b7a592714be9786277
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-02 18:51:02 -0600

    fixed the right way

--- src/state/actions.js
+++ src/state/actions.js
@@ -7,7 +7,7 @@ export const SHOW_TABLE = "SHOW_TABLE"
 export const EDIT_USER = "EDIT_USER"
 export const DELETE_USER = "DELETE_USER"
 
-export function loadUsers() {
+function loadUsers() {
   return (dispatch, getState, api) => {
     axios.get('https://5a747e5b61c2a40012894ab4.mockapi.io/api/v1/users')
       .then(({ data: users }) => {
@@ -16,29 +16,16 @@ export function loadUsers() {
   }
 }
 
-function getUsers() {
-  return (dispatch, getState, api) => {
-    axios.get('https://5a747e5b61c2a40012894ab4.mockapi.io/api/v1/users')
-      .then(({ data: users }) => {
-        console.log("in get users")
-        dispatch({type: USER_FETCH_SUCCESS, payload: users})
-      })
-  }
-}
 
-export function deleteUserFromList(userId){
+function deleteUserFromList(userId){
   return (dispatch, getState, api) => {
   axios.delete('https://5a747e5b61c2a40012894ab4.mockapi.io/api/v1/users/' + userId)
     .then((response) => {
-
       dispatch({type: DELETE_USER, payload: response.data})
-
-      axios.get('https://5a747e5b61c2a40012894ab4.mockapi.io/api/v1/users')
-      .then(({ data: users }) => {
-        console.log("in get users")
-        dispatch({type: USER_FETCH_SUCCESS, payload: users})
-      })
+      dispatch(loadUsers())
     }
     )
   }
 }
+
+export { loadUsers, deleteUserFromList }
